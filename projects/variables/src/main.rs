7c3fd7d4957a6eeedbcc143acca6a70b9b176c65
owl3670 <gnltjr123@gmail.com>
2023-05-31 13:12:02 +0900

    + Variables and Mutability

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
+    // cannot assign twice to immutable variable
+    
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
