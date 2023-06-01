d3b69fd787a760b909202fa9251a955b0dbc29aa
owl3670 <gnltjr123@gmail.com>
2023-06-01 09:07:04 +0900

    + Add comment

--- projects/data_types/src/main.rs
+++ projects/data_types/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     let guess: u32 = "42".parse().expect("Not a number!"); // It's okay
-    let guess = "42".parse().expect("Not a number!"); // error (type annotation needed)
+    // let guess = "42".parse().expect("Not a number!"); // error (type annotation needed)
 }
