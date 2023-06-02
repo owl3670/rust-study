89c3874300e2096bb5a1b3f338ee37b5f2a9a46d
owl3670 <gnltjr123@gmail.com>
2023-06-02 15:54:45 +0900

    + Add prefix to unused variable

--- projects/data_types/src/main.rs
+++ projects/data_types/src/main.rs
@@ -1,31 +1,31 @@
 fn main() {
-    let guess: u32 = "42".parse().expect("Not a number!"); // It's okay
+    let _guess: u32 = "42".parse().expect("Not a number!"); // It's okay
     // let guess = "42".parse().expect("Not a number!"); // error (type annotation needed)
 
     // Scalar Types
     // Integer Types
 
-    let int8: i8 = -10; 
-    let uint8: u8 = 10; 
+    let _int8: i8 = -10; 
+    let _uint8: u8 = 10; 
 
-    let int16: i16 = -10;
-    let uint16: u16 = 10;
+    let _int16: i16 = -10;
+    let _uint16: u16 = 10;
 
-    let int32: i32 = -10;
-    let uint32: u32 = 10;
+    let _int32: i32 = -10;
+    let _uint32: u32 = 10;
 
-    let int64: i64 = -10;
-    let uint64: u64 = 10;
+    let _int64: i64 = -10;
+    let _uint64: u64 = 10;
 
-    let int128: i128 = -10;
-    let uint128: u128 = 10;
+    let _int128: i128 = -10;
+    let _uint128: u128 = 10;
 
-    let arch: isize = -10; // depends on the architecture of the computer
-    let uarch: usize = 10;
+    let _arch: isize = -10; // depends on the architecture of the computer
+    let _uarch: usize = 10;
 
-    let dec: i32 = 100_000;
-    let hex: i32 = 0xff;
-    let octal: i32 = 0o77;
-    let binary: i32 = 0b1111_0000;
-    let byte: u8 = b'A'; // u8 only
+    let _dec: i32 = 100_000;
+    let _hex: i32 = 0xff;
+    let _octal: i32 = 0o77;
+    let _binary: i32 = 0b1111_0000;
+    let _byte: u8 = b'A'; // u8 only
 }
