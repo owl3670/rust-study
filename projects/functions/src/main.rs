2ec9e14f66e81171799795bd30cca8218997e41d
owl3670 <gnltjr123@gmail.com>
2023-06-09 14:22:12 +0900

    + Parameters

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -2,8 +2,18 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    another_param_function(30);
+    add(3, 5);
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn another_param_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn add(a: i32, b: i32){
+    println!("{a} + {b} = {}", a+b);
 }
