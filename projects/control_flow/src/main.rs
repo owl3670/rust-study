58e43a4a1bb0d173c2364feb392abb5dcc42d65d
owl3670 <gnltjr123@gmail.com>
2023-06-14 08:52:27 +0900

    + if Expressions

--- projects/control_flow/src/main.rs
+++ projects/control_flow/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // if expressions
+    let number = 6;
+
+    if number % 5 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    // let number = if condition { 5 } else { "six"}; // Error occurred
+    println!("The value of number is: {number}");
 }
