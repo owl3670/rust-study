09ff2927e0c876d3475a3d51eed6ad399928d5d9
owl3670 <gnltjr123@gmail.com>
2023-06-15 15:48:28 +0900

    + while, for

--- projects/control_flow/src/main.rs
+++ projects/control_flow/src/main.rs
@@ -48,4 +48,26 @@ fn main() {
         count += 1;
     }
     println!("End count = {count}");
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+
+        number -= 1;
+    }
+    println!("End of while");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
+    }
+    for element in a {
+        println!("The value is: {element}");
+    }
+
+    for number in (1..4).rev(){
+        println!("{number}");
+    }
 }
