adf80cba1efde99a490088d6c4b50747bf499831
owl3670 <gnltjr123@gmail.com>
2023-06-15 15:03:43 +0900

    + Loop

--- projects/control_flow/src/main.rs
+++ projects/control_flow/src/main.rs
@@ -16,4 +16,36 @@ fn main() {
     let number = if condition { 5 } else { 6 };
     // let number = if condition { 5 } else { "six"}; // Error occurred
     println!("The value of number is: {number}");
+
+    // Repetition with Loops
+    let mut counter = 0;
+    let result = loop {
+        println!("again!");
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
